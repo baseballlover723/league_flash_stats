fe6bdd09a3354b8ce1e5f644599f5c24dc20b346
baseballlover723 <rosspa@rose-hulman.edu>
2015-08-28 17:45:09 -0400

    stuf

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,19 @@
 class AdminController < ApplicationController
+  @@polling = false
   def index
+    @var = 5
+  end
+
+  def start_stop_polling
+    puts "start_stop_polling"
+  end
+
+  def start_polling
+    puts "already polling" and return if @@polling
+    data = File.read("/path/to/file")
+  end
+
+  def stop_polling
+    puts "not polling" and return unless @@polling
   end
 end

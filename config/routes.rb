6a6f36369ff05389b24f1635d4618d08aad55d46
baseballlover723 <rosspa@rose-hulman.edu>
2015-08-28 15:44:32 -0400

    changed results to champ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'champ/show'
+
+  get 'champ/index'
+
+  get 'champ/create'
+
   get 'results/show'
 
   # The priority is based upon order of creation: first created -> highest priority.

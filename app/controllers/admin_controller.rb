4d23bcb81dd5cd4129e3c06def1e42397b8b17b7
baseballlover723 <rosspa@rose-hulman.edu>
2015-08-29 18:40:40 -0400

    does the first match

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,9 @@
 require 'csv'
 class AdminController < ApplicationController
   @@polling = false
-  @@base_request = "global.api.pvp.net"
-  @@champion_request_path = "/api/lol/static-data/na/v1.2/champion"
+  @@base_request = "na.api.pvp.net"
+  @@request_path = "/api/lol/na/v2.2/match/"
+  @@match_id = 1907069332
 
   def index
     @start_stop_string = @@polling ? "Stop" : "Start"
@@ -34,22 +35,43 @@ class AdminController < ApplicationController
     puts "polling"
     while @@polling do
       keys.each do |key|
-        champion_uri = URI::HTTPS.build(host: base_champion_request,
-                                        path: champion_request_path,
-                                        query: {champData: "image", api_key: keys[0]}.to_query)
-        response = HTTParty.get(champion_uri, verify: false).parsed_response
-        champions = response["data"]
+        match_uri = URI::HTTPS.build(host: @@base_request,
+                                     path: @@request_path + get_next_match_id,
+                                     query: {api_key: key}.to_query)
+        response = HTTParty.get(match_uri, verify: false).parsed_response
+        response["participants"].each do |participant|
+          rank = participant["highestAchievedSeasonTier"].downcase
+          champion_id = participant["championId"]
+          lane = parse_lane participant["timeline"]["lane"].downcase
 
-      end
+          won = participant["stats"]["winner"]
+          flash_on_d = participant["spell1Id"] == 4
+          flash_on_f = participant["spell2Id"] == 4
 
 
-      recent_posts = HTTParty.get "http://www.example.org/?json=get_recent_posts"
-      puts recent_posts.status, recent_posts.count
+          has_flash = flash_on_d || flash_on_f
 
-      recent_posts.posts.each do |post|
-        puts post.title #guessing
+          champion_lane = ChampionLane.find_by(champion_id: champion_id, lane: lane)
+          rank = Rank.find_by(champion_lane: champion_lane, rank: rank, has_flash: has_flash, flash_on_f: flash_on_f)
+          if won
+            rank.wins += 1
+          else
+            rank.losses += 1
+          end
+          rank.save!
+        end
+        return
       end
     end
   end
-end
 
+  def get_next_match_id
+    @@match_id.to_s
+  end
+
+  def parse_lane(lane)
+    return "bot" if lane == "bottom"
+    return "mid" if lane == "middle"
+    lane
+  end
+end

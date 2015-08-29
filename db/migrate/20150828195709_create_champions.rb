0c3b755671ba6c1b87b83a20146ac925c8a808d6
baseballlover723 <rosspa@rose-hulman.edu>
2015-08-29 01:16:10 -0400

    got rid of champion id

--- db/migrate/20150828195709_create_champions.rb
+++ db/migrate/20150828195709_create_champions.rb
@@ -1,7 +1,6 @@
 class CreateChampions < ActiveRecord::Migration
   def change
     create_table :champions do |t|
-      t.integer :champion_id
       t.string :name
       t.string :title
       t.string :img

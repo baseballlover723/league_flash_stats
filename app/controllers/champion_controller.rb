57c2b10d52b6dd0cf8cbd14edbe393c20df244d6
Andrew Ma <maastergotmail@gmail.com>
2015-08-29 16:50:11 -0400

    champs and pics

--- app/controllers/champion_controller.rb
+++ app/controllers/champion_controller.rb
@@ -3,8 +3,8 @@ class ChampionController < ApplicationController
   end
 
   def index
-    @riot_path = "https://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/"
-    @champions = Champion.all
+    @riot_path = "https://ddragon.leagueoflegends.com/cdn/5.16.1/img/champion/"
+    @champions = Champion.all.sort_by{ |champion| champion.name }
   end
 
   def create

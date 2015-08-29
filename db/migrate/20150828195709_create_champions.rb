8a6757ad2bce451c9c09707ae0497c8542b8d9be
baseballlover723 <rosspa@rose-hulman.edu>
2015-08-29 18:26:33 -0400

    added the splash and loading img links to champion

--- db/migrate/20150828195709_create_champions.rb
+++ db/migrate/20150828195709_create_champions.rb
@@ -4,6 +4,8 @@ class CreateChampions < ActiveRecord::Migration
       t.string :name
       t.string :title
       t.string :img
+      t.string :splash_img
+      t.string :loading_img
       t.timestamps null: false
     end
   end

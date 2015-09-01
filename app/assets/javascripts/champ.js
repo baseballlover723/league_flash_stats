caa5591780f27725d5d8f72fab7d5e1084169c3e
baseballlover723 <rosspa@rose-hulman.edu>
2015-09-01 00:53:38 -0400

    fixed typos and polished a bit

--- app/assets/javascripts/champ.js
+++ app/assets/javascripts/champ.js
@@ -26,7 +26,7 @@ var chartOptions = {
   animateScale : false,
 
   //String - A legend template
-  legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>",
+  legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
 
 };
 
